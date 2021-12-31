723b19c4edbb9bd4b4679b911a1e0c2e001b4584
Claudio Ciano <shapelessguy@gmail.com>
2021-12-31 18:52:59 +0100

    Add project files.

---
+++ Application.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CyanLauncherManager
+{
+    class LApplication
+    {
+        public string name = "";
+        public string apps_path = "";
+        public string icon_path = "";
+        public string id = "";
+
+        public LApplication(string directory, string icon_path, string id)
+        {
+            this.name = Path.GetFileName(directory);
+            this.apps_path = Path.GetDirectoryName(directory);
+            this.icon_path = icon_path;
+            if (id.Length > 0) this.id = id;
+            else
+            {
+                this.icon_path = this.icon_path.Replace(".ico", "default.ico");
+                this.id = "default";
+                string new_icon_path = Path.Combine(apps_path, "__Icons__", this.id + ".ico");
+                string reference_path = Path.Combine(apps_path, name, name + ".exe");
+                string loc_path = Path.Combine(apps_path, name, name + ".lnk");
+                using(StreamWriter stream = new StreamWriter(Path.Combine(apps_path, name, "id.txt"))) stream.Write(this.id);
+                new CreateLink(reference_path, loc_path, new_icon_path);
+            }
+        }
+
+        private bool UpdateName(string new_name)
+        {
+            if (name == new_name) return true;
+            Console.WriteLine("Updating name.");
+            try
+            {
+                Program.KillProc(name);
+
+                Directory.Move(Path.Combine(apps_path, name), Path.Combine(apps_path, new_name));
+                try
+                {
+                    File.Move(Path.Combine(apps_path, new_name, name + ".exe"), Path.Combine(apps_path, new_name, new_name + ".exe"));
+                    File.Move(Path.Combine(apps_path, new_name, name + ".lnk"), Path.Combine(apps_path, new_name, new_name + ".lnk"));
+                }
+                catch (Exception) { Directory.Move(Path.Combine(apps_path, new_name), Path.Combine(apps_path, name)); return false; }
+
+                this.name = new_name;
+                return true;
+            }
+            catch (Exception ex) { Console.WriteLine(ex); }
+            return false;
+        }
+        private bool UpdateImage(string ico_path)
+        {
+            Console.WriteLine("Updating image.");
+            try
+            {
+                Process[] processCollection = Process.GetProcesses();
+                foreach (Process p in processCollection) if (p.ProcessName == name) p.Kill();
+                foreach (string file in Directory.EnumerateFiles(Path.Combine(apps_path, name))) if (Program.isLink(file)) File.Delete(file);
+
+                try {
+                    id = Program.GenerateCausualString(10);
+                    string new_icon_path = Path.Combine(apps_path, "__Icons__", id + ".ico");
+                    string reference_path = Path.Combine(apps_path, name, name + ".exe");
+                    string loc_path = Path.Combine(apps_path, name, name + ".lnk");
+                    if (Path.GetExtension(ico_path) == "ico") File.Copy(ico_path, new_icon_path); 
+                    else PngIconConverter.Convert(ico_path, new_icon_path, 200);
+                    using (StreamWriter stream = new StreamWriter(Path.Combine(apps_path, name, "id.txt"))) stream.Write(id);
+                    new CreateLink(reference_path, loc_path, new_icon_path);
+                }
+                catch (Exception) { }
+
+                this.icon_path = ico_path;
+                return true;
+            }
+            catch (Exception ex) { Console.WriteLine(ex); }
+            return false;
+        }
+
+
+        public bool setName(string name)
+        {
+            bool valid = true;
+            try { Path.GetFullPath(Path.Combine(apps_path, name)); }
+            catch (Exception) { valid = false; }
+            if (name.Contains(".")) valid = false;
+
+            if (valid)
+            {
+                UpdateName(name);
+                return true;
+            }
+            else
+            {
+                MessageBox.Show("The application name is invalid.");
+                return false;
+            }
+        }
+        public bool setImage(string ico_path)
+        {
+            return UpdateImage(ico_path);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CyanLauncherManager
+{
+    static class Program
+    {
+        /// <summary>
+        /// Punto di ingresso principale dell'applicazione.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new MainWindow());
+        }
+        static public bool isIcon(string file)
+        {
+            if (file.Contains(".ico")) return true;
+            else return false;
+        }
+        static public bool isLink(string file)
+        {
+            if (file.Contains(".lnk")) return true;
+            else return false;
+        }
+        static public bool isText(string file)
+        {
+            if (file.Contains(".txt")) return true;
+            else return false;
+        }
+
+        static public void KillProc(string name)
+        {
+            Process[] processCollection = Process.GetProcesses();
+            foreach (Process p in processCollection) if (p.ProcessName == name) p.Kill();
+            System.Threading.Thread.Sleep(100);
+        }
+
+        static public List<string> GetApplicationNames(string path, bool only_name = false)
+        {
+            List<string> output = new List<string>();
+            foreach (string directory in Directory.EnumerateDirectories(path))
+            {
+                if (directory != Path.Combine(path, "__Icons__") && directory != Path.Combine(path, "__Matrix__"))
+                {
+                    output.Add(only_name ? Path.GetFileName(directory) : directory);
+                }
+            }
+            return output;
+        }
+
+        static public string GenerateCausualString(int length)
+        {
+            Random random = new Random();
+            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+            return new string(Enumerable.Repeat(chars, length).Select(s => s[random.Next(s.Length)]).ToArray());
+        }
+    }
+}
